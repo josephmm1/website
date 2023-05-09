2eda6705bbd4c2e9c356b75fd2674aeff9356602
josephLocal <joseph.meagor@yahoo.co.uk>
2023-05-09 22:49:05 +0100

    query DB

--- main/php/database.php
+++ main/php/database.php
@@ -4,10 +4,16 @@ echo "php data ";
 $config = parse_ini_file('/home/dvm33ugq28ol/config/config.ini');
 
 $connection = mysqli_connect("localhost",$config['username'],$config['pass'],$config['dbName']);
+$postAmountQuery = "SELECT COUNT(*) as count FROM Posts;"
+$postAmount = $connection->query($postAmountQuery)
 
+$postQuery = "SELECT * FROM Posts"
+$post = $connection->query($postQuery)
 if ($connection)
 {
 echo "<p>DB connected</p>";
+echo "<p>we have $postAmount posts</p>"
+echo "<p>$post</p>"
 }
 else
 {
