f4394e3c70f57fe25ffd31caaabc12e7a7fa6c49
josephLocal <joseph.meagor@yahoo.co.uk>
2023-05-09 23:01:59 +0100

    cleanup

--- main/php/database.php
+++ main/php/database.php
@@ -3,8 +3,6 @@
 $config = parse_ini_file('/home/dvm33ugq28ol/config/config.ini');
 
 $connection = mysqli_connect("localhost",$config['username'],$config['pass'],$config['dbName']);
-$postAmountQuery = "SELECT COUNT(*) as count FROM Posts;";
-$postAmount = $connection->query($postAmountQuery);
 
 $postQuery = "SELECT * FROM Posts";
 $post = $connection->query($postQuery);
@@ -12,7 +10,9 @@ if ($connection){
     echo "<p>DB connected</p>";
     if (mysqli_num_rows($post) > 0) {
         while($row = mysqli_fetch_assoc($post)) {
-            echo "id: " . $row["id"]. " - title: " . $row["title"]. " - content: " . $row["content"]. " - timestamp".$row["timestamp"]."<br>";
+        echo "<h3>".$row["title"]."</h3>"
+        echo "<p>".$row["content"]."</p>"
+        echo "<p>posted at: ".$row["timestamp"]."</p>"
         }
     } else {
     echo "0 results";
