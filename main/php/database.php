60b4c36ebe9f1d8b93f329a44240b100d7e8a288
josephLocal <joseph.meagor@yahoo.co.uk>
2023-05-22 21:17:12 +0100

    fix php

--- main/php/database.php
+++ main/php/database.php
@@ -1,17 +1,18 @@
 <?php
 
 $config = parse_ini_file('/home/dvm33ugq28ol/config/config.ini');
-
 $connection = mysqli_connect("localhost",$config['username'],$config['pass'],$config['dbName']);
-
 $postQuery = "SELECT * FROM Posts";
 $post = $connection->query($postQuery);
+
 if ($connection){
     if (mysqli_num_rows($post) > 0) {
         while($row = mysqli_fetch_assoc($post)) {
+        echo "<div class='center border'>";
         echo "<h3>".$row["title"]."</h3>";
         echo "<p>".$row["content"]."</p>";
         echo "<p>posted at: ".$row["timestamp"]."</p>";
+        echo "</div>";
         }
     } else {
     echo "<p>posts not found</p>";
