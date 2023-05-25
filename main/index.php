40bccaec1e2c5fddb0159222af06f6d65159688e
josephLocal <joseph.meagor@yahoo.co.uk>
2023-05-25 21:26:07 +0100

    Add code snippets

--- main/index.php
+++ main/index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-    <link rel="stylesheet" href="/css/style.css?ver=1.0.2">
+    <link rel="stylesheet" href="/css/style.css?ver=1.0.3">
     <script src="js/script.js"></script>
     <body>
         <div class="header" id="myHeader">
