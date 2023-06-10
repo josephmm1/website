bb128d1385b43a5f6bcde4571f005a631dd9c77b
josephLocal <joseph.meagor@yahoo.co.uk>
2023-06-10 22:40:13 +0100

    update nav

--- main/index.php
+++ main/index.php
@@ -1,21 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
-    <link rel="stylesheet" href="/css/style.css?ver=1.1.1">
+    <link rel="stylesheet" href="/css/style.css?ver=1.1.2">
     <script src="js/script.js"></script>
     <body>
         <div class="header flex" id="myHeader">
         <div>This is a logo WIP</div>
             <ul class="navigation">
-                <li>
-                    <div class="navItem">Questions
+                <li class="nav-item">
+                    <div>Questions
                     </div>
                 </li>
-                <li>
-                    <div class="navItem">Apps
+                <li class="nav-item">
+                    <div>Apps
                     </div>
                 </li>
-                <li>
-                    <div class="navItem">About
+                <li class="nav-item">
+                    <div>About
                     </div>
                 </li>
             </ul>
