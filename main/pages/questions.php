ff6fe0af20963f0b72f5e15b739683b2f21fd1ce
josephLocal <joseph.meagor@yahoo.co.uk>
2023-06-26 14:09:31 +0100

    add body

--- main/pages/questions.php
+++ main/pages/questions.php
@@ -1,17 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
-<div class="center border">
-    <form>
-        <input type="text">
-        <input type="submit">
-    </form>
-</div>
-<div class="spaceDiv">
-</div>
-<div>
-     <?
-        include("php/questionDB.php");
-     ?>
-</div>
-<div class="spaceDiv">
-</div>
+<body>
+    <div class="center border">
+        <form>
+            <input type="text">
+            <input type="submit">
+        </form>
+    </div>
+    <div class="spaceDiv">
+    </div>
+    <div>
+         <?
+            include("php/questionDB.php");
+         ?>
+    </div>
+    <div class="spaceDiv">
+    </div>
+</body>
