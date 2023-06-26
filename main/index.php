b801a7e7a5f8737408d5727c6aa97ae7bc4bcdfd
josephLocal <joseph.meagor@yahoo.co.uk>
2023-06-26 14:15:26 +0100

    add body

--- main/index.php
+++ main/index.php
@@ -1,32 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
-    <link rel="stylesheet" href="/css/style.css?ver=1.1.4">
+    <link rel="stylesheet" href="/css/style.css?ver=1.1.3">
     <script src="js/script.js"></script>
     <body>
-        <div class="header flex" id="myHeader">
-            <div>This is a logo WIP</div>
-                <ul class="navigation">
-                    <li class="nav-item">
-                        <div>Questions
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <div>Apps
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <div>About
-                        </div>
-                    </li>
-                </ul>
+    <div id="nav-placeholder">
+
+    </div>
+        <div class="spaceDiv">
+        </div>
+        <div class="center border">
+            <form>
+                <input type="text">
+                <input type="submit">
+            </form>
         </div>
         <div class="spaceDiv">
         </div>
-
-            <iframe src="pages/questions.php" title="description" class="frame"></iframe>
-
+        <div>
+             <?
+                include("php/database.php");
+             ?>
+        </div>
+        <div class="spaceDiv">
+        </div>
         <div class="footer">
             <p>some text</p>
         </div>
+        <script>
+        $(function(){
+          $("#navbar-placeholder").load("pages/navigation.html");
+        });
+        </script>
     </body>
 </html>
