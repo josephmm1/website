7070c94c0e1d179a5cbf6f3aa4ab5b495b5e9803
josephLocal <joseph.meagor@yahoo.co.uk>
2023-06-26 14:46:40 +0100

    style nav bar

--- main/index.php
+++ main/index.php
@@ -1,34 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
-    <script src="js/script.js"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-    <body>
-        <div id="navbar-placeholder">
-        </div>
-        <div class="spaceDiv">
-        </div>
-        <div class="center border">
-            <form>
-                <input type="text">
-                <input type="submit">
-            </form>
-        </div>
-        <div class="spaceDiv">
-        </div>
-        <div>
-             <?
-                include("php/database.php");
-             ?>
-        </div>
-        <div class="spaceDiv">
-        </div>
-        <div class="footer">
-            <p>some text</p>
-        </div>
-        <script>
-        $(function(){
-          $("#navbar-placeholder").load("navigation.html");
-        });
-        </script>
-    </body>
+<link rel="stylesheet" href="/css/style.css?ver=1.1.4">
+<script src="js/script.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+<body>
+    <div id="navbar-placeholder">
+    </div>
+    <div class="spaceDiv">
+    </div>
+    <div class="center border">
+        <form>
+            <input type="text">
+            <input type="submit">
+        </form>
+    </div>
+    <div class="spaceDiv">
+    </div>
+    <div>
+         <?
+            include("php/database.php");
+         ?>
+    </div>
+    <div class="spaceDiv">
+    </div>
+    <div class="footer">
+        <p>some text</p>
+    </div>
+    <script>
+    $(function(){
+      $("#navbar-placeholder").load("navigation.html");
+    });
+    </script>
+</body>
 </html>
