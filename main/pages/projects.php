2c2e1ed6a2cbd6f26c6446cbfe3ce5b91b11f4d4
josephLocal <joseph.meagor@yahoo.co.uk>
2023-06-26 14:21:24 +0100

    add project Page

---
+++ main/pages/projects.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+    <div id="navbar-placeholder">
+    </div>
+    <div>
+        <p>Project Page<p>
+    </div>
+    <script>
+    $(function(){
+      $("#navbar-placeholder").load("pages/navigation.html");
+    });
+    </script>
+</body>
