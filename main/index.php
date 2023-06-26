788dfeb7a4b0df7bd0eb65236605ecf582661b3f
josephLocal <joseph.meagor@yahoo.co.uk>
2023-06-26 14:48:16 +0100

    updfate style vbersion

--- main/index.php
+++ main/index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<link rel="stylesheet" href="/css/style.css?ver=1.1.4">
+<link rel="stylesheet" href="/css/style.css?ver=1.1.5">
 <script src="js/script.js"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 <body>
